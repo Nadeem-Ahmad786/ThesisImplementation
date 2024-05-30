1550a2442dda5def2b69b90263a2ba03ca3e8c7e
Nadeem Ahmad <nadeemgoogl786@gmail.com>
2024-05-30 15:12:19 +0530

    added block tag functionality and change router

--- routes/video.js
+++ routes/video.js
@@ -7,8 +7,12 @@ const { authenticateJWT } = require('../middlewares/authenticator');
 router.post('/upload', authenticateJWT, upload.single('video'), videoController.create);
 router.get('/currentUser', authenticateJWT, videoController.readCurrentUserVideo);
 router.get('/watch/:videoId', authenticateJWT, videoController.watchVideo)
-router.get('/:id', videoController.readSingle)
+router.get('/singleRead/:id', authenticateJWT, videoController.readSingle)
 router.post('/filter', authenticateJWT, videoController.filterVideos);
 router.post('/block-tag', authenticateJWT, videoController.blockTag);
+router.post('/unblock-tag', authenticateJWT, videoController.unblockTag);
+router.get('/view-blocked-tags', authenticateJWT, videoController.viewBlockedTags);
 router.get('/recent-tags', authenticateJWT, videoController.recentlyWatchedTags);
+router.get('/top-watch', authenticateJWT, videoController.topWatch)
+
 module.exports = router;
